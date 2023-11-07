3eb587525b4bd80cba7c6175a2a945188d2b493a
turbou <turbou@i.softbank.jp>
2023-11-07 19:30:04 +0900

    update.

--- work/java_rlsnote_new.py
+++ work/java_rlsnote_new.py
@@ -3,8 +3,10 @@ from bs4 import BeautifulSoup
 import xml.dom.minidom
 from django.utils.feedgenerator import Rss201rev2Feed
 from datetime import datetime
+import locale
 
 def main():
+    locale.setlocale(locale.LC_TIME, "C")
     url = 'https://docs.contrastsecurity.jp/ja/java-agent-release-notes-and-archive.html'
     res = req.urlopen(url)
     soup = BeautifulSoup(res, 'lxml')
@@ -23,12 +25,17 @@ def main():
     for elem in elems:
         try:
             id_str = elem.get("id")
+            #pubdate_str = elem.get("data-publication-date") # November 6, 2023
+            pubdate_str = elem.get("data-time-modified") # November 6, 2023
+            pubdate = None
+            if pubdate_str:
+                pubdate = datetime.strptime(pubdate_str, '%B %d, %Y')
             title = elem.select('h3.title')[0].text.strip()
             if not title.lower().startswith('java'):
                 continue
             desc = elem.select('div.panel-body')[0].text
             url = 'https://docs.contrastsecurity.jp/ja/java-agent-release-notes-and-archive.html#%s' % id_str
-            feed.add_item(title=title, link=url, description=desc)
+            feed.add_item(title=title, link=url, description=desc, pubdate=pubdate)
         except IndexError:
             continue
 
