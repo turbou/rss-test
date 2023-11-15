42b1f83b075fb9712f4172cad00dc00f8c5b07dc
turbou <turbou@i.softbank.jp>
2023-11-16 08:47:05 +0900

    import.

---
+++ work/java_support_tech.py
@@ -0,0 +1,75 @@
+import urllib.request as req 
+from bs4 import BeautifulSoup
+import xml.dom.minidom
+from django.utils.feedgenerator import Rss201rev2Feed
+from datetime import datetime
+import locale
+import re
+import difflib
+
+def main():
+    url = 'https://docs.contrastsecurity.jp/ja/java-supported-technologies.html'
+    res = req.urlopen(url)
+    soup = BeautifulSoup(res, 'lxml')
+    elems = soup.select('section.section')
+    modified_date = soup.select_one('span.formatted-date').text.strip()
+    print(modified_date)
+
+    feed = Rss201rev2Feed(
+        title='Java Supported Technologies',
+        link='https://contrastsecurity.dev/contrast-documentation-rss',
+        description='Java Supported Technologies',
+        language='ja',
+        author_name="Contrast Security Japan G.K.",
+        feed_url='https://contrastsecurity.dev/contrast-documentation-rss/java_support_tech_update.xml',
+        feed_copyright='Copyright 2023 Contrast Security Japan G.K.'
+    )
+
+    write_flg = False
+    item_dict = {}
+    pubdate = None
+    for elem in elems:
+        try:
+            if elem.parent.name == 'div':
+                pubdate_str = elem.get("data-time-modified") # November 6, 2023
+                if pubdate_str:
+                    pubdate = datetime.strptime(pubdate_str, '%B %d, %Y')
+                continue
+            id_str = elem.get("id")
+            url = 'https://docs.contrastsecurity.jp/ja/java-supported-technologies.html#%s' % id_str
+            title = elem.select_one('h2.title').text
+            print(title)
+            #print('----------------------------------------------------------')
+            text_buffer = []
+            for elem2 in elem.select('p'):
+                text_buffer.append(elem2.text)
+            #print('----------------------------------------------------------')
+            with open('/files/%s.tmp' % title,'w') as fp:
+                fp.write('\n'.join(text_buffer))
+            before_text = None
+            with open('/files/%s.txt' % title,'r') as fp:
+                before_text = fp.readlines()
+            after_text = None
+            with open('/files/%s.tmp' % title,'r') as fp:
+                after_text = fp.readlines()
+            res = difflib.unified_diff(after_text, before_text)
+            res_str = '\n'.join(res)
+            if (len(res_str.strip()) > 0):
+                item_dict[title] = (url, res_str)
+        except IndexError:
+            continue
+
+    if pubdate is None:
+        pubdate = datetime.date.today()
+    for k, v in item_dict.items():
+        feed.add_item(title=k, link=v[0], description=''.join(['<p>{0}</p>'.format(s) for s in v[1].splitlines()]), pubdate=pubdate)
+
+    if len(item_dict):
+        str_val = feed.writeString('utf-8')
+        dom = xml.dom.minidom.parseString(str_val)
+        with open('/feeds/java_support_tech_update.xml','w') as fp: 
+            dom.writexml(fp, encoding='utf-8', newl='\n', indent='', addindent='    ')
+
+if __name__ == "__main__":
+    main()
+
