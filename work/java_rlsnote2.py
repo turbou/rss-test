ad5e94b7be9de569a3bccc949805b2db9d14b1b1
Taka Shiozaki <taka.shiozaki@contrastsecurity.com>
2024-08-19 23:46:49 +0900

    update.

--- work/java_rlsnote2.py
+++ work/java_rlsnote2.py
@@ -25,36 +25,6 @@ def main():
         feed_url='https://contrastsecurity.dev/contrast-documentation-rss/java_rlsnote.xml',
         feed_copyright='Copyright 2023 Contrast Security Japan G.K.'
     )
-    for idx, elem in enumerate(elems):
-        try:
-            id_str = elem.get("id")
-            #pubdate_str = elem.get("data-publication-date") # November 6, 2023
-            pubdate_str = elem.get("data-time-modified") # November 6, 2023
-            pubdate = None
-            if pubdate_str:
-                pubdate = datetime.strptime(pubdate_str, '%B %d, %Y')
-            title = elem.select('h3.title')[0].text.strip()
-            if not title.lower().startswith('java'):
-                continue
-            #desc = elem.select('div.panel-body')[0].text
-            desc_buffer = []
-            #for elem2 in elem.select('p, div'):
-            for elem2 in elem.select('p'):
-                if elem2.parent.name == 'li':
-                    desc_buffer.append('・%s' % elem2.text)
-                else:
-                    desc_buffer.append('%s' % elem2.text)
-                #print(elem2.text)
-            id_hash = hashlib.md5(id_str.encode()).hexdigest()
-            url = 'https://docs.contrastsecurity.jp/ja/java-agent-release-notes-and-archive.html#%s' % id_str
-            guid = 'https://docs.contrastsecurity.jp/ja/java-agent-release-notes-and-archive.html#%s' % id_hash
-            if not 'リリース日' in ''.join(desc_buffer):
-                continue
-            print(idx)
-            feed.add_item(title=title, link=url, description=''.join(['<p>{0}</p>'.format(s) for s in desc_buffer]), pubdate=pubdate, unique_id=guid)
-        except IndexError:
-            continue
-
     elem = elems[0]
     error_flg = False
     try:
@@ -88,6 +58,36 @@ def main():
             feed.add_item(title=title, link=url, description=''.join(['<p>{0}</p>'.format(s) for s in desc_buffer]), pubdate=pubdate, unique_id=guid)
     except IndexError:
         print('error')
+
+    for idx, elem in enumerate(elems):
+        try:
+            id_str = elem.get("id")
+            #pubdate_str = elem.get("data-publication-date") # November 6, 2023
+            pubdate_str = elem.get("data-time-modified") # November 6, 2023
+            pubdate = None
+            if pubdate_str:
+                pubdate = datetime.strptime(pubdate_str, '%B %d, %Y')
+            title = elem.select('h3.title')[0].text.strip()
+            if not title.lower().startswith('java'):
+                continue
+            #desc = elem.select('div.panel-body')[0].text
+            desc_buffer = []
+            #for elem2 in elem.select('p, div'):
+            for elem2 in elem.select('p'):
+                if elem2.parent.name == 'li':
+                    desc_buffer.append('・%s' % elem2.text)
+                else:
+                    desc_buffer.append('%s' % elem2.text)
+                #print(elem2.text)
+            id_hash = hashlib.md5(id_str.encode()).hexdigest()
+            url = 'https://docs.contrastsecurity.jp/ja/java-agent-release-notes-and-archive.html#%s' % id_str
+            guid = 'https://docs.contrastsecurity.jp/ja/java-agent-release-notes-and-archive.html#%s' % id_hash
+            if not 'リリース日' in ''.join(desc_buffer):
+                continue
+            print(idx)
+            feed.add_item(title=title, link=url, description=''.join(['<p>{0}</p>'.format(s) for s in desc_buffer]), pubdate=pubdate, unique_id=guid)
+        except IndexError:
+            continue
 
     str_val = feed.writeString('utf-8')
     dom = xml.dom.minidom.parseString(str_val)
